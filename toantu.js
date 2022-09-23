a10125fa13446c1588b126e54a74a8ae5b4dbcc2
trilinh2000 <114172972+trilinh2000@users.noreply.github.com>
2022-09-23 21:09:01 +0700

    Update and rename bai1 to toantu.js

---
+++ toantu.js
@@ -0,0 +1,6 @@
+let inputWeight=prompt("nhap a");
+let inputHeight=prompt("nhap b");
+let weight=parseInt(inputWeight);
+let height=parseInt(inputHeight);
+let are=weight*height;
+document.write("are: "+are);
